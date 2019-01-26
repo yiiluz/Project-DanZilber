eeac06f717dc34cabfa3ffd399da5667357997d4
Yitzhak Iluz <Yitzhak.Iluz@gmail.com>
2019-01-26 21:44:33 +0200

    bla bla

--- BL/DL/DAL_XML_imp.cs
+++ BL/DL/DAL_XML_imp.cs
@@ -365,6 +365,7 @@ namespace DL
                           IsTesterUpdateStatus = Convert.ToBoolean(item.Element("Test").Element("IsTesterUpdateStatus").Value),
                           IsTestAborted = Convert.ToBoolean(item.Element("Test").Element("IsTestAborted").Value)
                       }).ToList();
+
             TestsRoot.Save(TestsRootPath);
             return it;
         }
