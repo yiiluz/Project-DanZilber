87cd469692fcb71c6a51054b081e9274deee8ab9
Yitzhak Iluz <Yitzhak.Iluz@gmail.com>
2019-02-08 03:33:54 +0200

    העטהטע

--- BL/UI_Ver2/GetIDWindow.xaml.cs
+++ BL/UI_Ver2/GetIDWindow.xaml.cs
@@ -19,14 +19,13 @@ namespace UI_Ver2
     /// </summary>
     public partial class GetIDWindow : Window
     {
-        Window parent;
         string type;
         bool isClosedByButton = false;
         public bool IsClosedByButton { get => isClosedByButton; set => isClosedByButton = value; }
 
-        public GetIDWindow(Window parent = null, string type = "")
+        public GetIDWindow(string type = "")
         {
-            this.parent = parent;
+            this.FlowDirection = FlowDirection.RightToLeft;
             this.type = type;
             WindowStartupLocation = WindowStartupLocation.CenterScreen;
             InitializeComponent();
@@ -43,7 +42,7 @@ namespace UI_Ver2
             if (TxtBx_ID.Text.Length != TxtBx_ID.MaxLength || !TxtBx_ID.Text.All(char.IsDigit))
             {
                 TxtBx_ID.Background = Brushes.Red;
-                MessageBox.Show("ID must be exactly 9 Digitis, and Digits only.", "Wrong input", MessageBoxButton.OK, MessageBoxImage.Information);
+                MessageBox.Show("מספר תעודת הזהות צריך להיות 9 ספרות בדיוק.", "קלט שגוי", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.None, MessageBoxOptions.RtlReading | MessageBoxOptions.RightAlign);
             }
             else
             {
@@ -57,11 +56,10 @@ namespace UI_Ver2
                         }
                         catch (Exception ex)
                         {
-                            MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                            MessageBox.Show(ex.Message, "שגיאה פנימית", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.None, MessageBoxOptions.RtlReading | MessageBoxOptions.RightAlign);
                             return;
                         }
                         Close();
-                        parent.Close();
                         break;
                     case "Trainee":
                         Trainee trainee;
@@ -71,18 +69,17 @@ namespace UI_Ver2
                         }
                         catch (Exception ex)
                         {
-                            MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                            MessageBox.Show(ex.Message, "שגיאה פנימית", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.None, MessageBoxOptions.RtlReading | MessageBoxOptions.RightAlign);
                             return;
                         }
                         Close();
-                        parent.Close();
                         break;
                     case "":
                         isClosedByButton = true;
                         Close();
                         break;
                     default:
-                        MessageBox.Show("Internal error. file GetIDWindow, case not exist on switch", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        MessageBox.Show("שגיאה פנימית. case לא קיים", "שגיאה פנימית", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.None, MessageBoxOptions.RtlReading | MessageBoxOptions.RightAlign);
                         break;
                 }
                 
